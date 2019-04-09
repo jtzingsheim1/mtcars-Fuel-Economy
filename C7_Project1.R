d3294409da516fd3b2f2812cb01e998fcbe78941
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-04-08 19:14:16 -0500

    Create initial script to begin working on project

---
+++ C7_Project1.R
@@ -0,0 +1,24 @@
+# Coursera Data Science Specialization Course 7 Project 1 Script
+
+
+# The purpose of this script is to complete the basic requirements behind the
+# project 1 peer-graded assignment which is part of the Regression Models course
+# from Johns Hopkins University within the Data Science Specialization on
+# Coursera.
+#
+# The instructions say to perform exploratory data analysis and use regression
+# models with the mtcars data set to answer these questions:
+# 1. Is an automatic or manual transmission better for fuel economy
+# 2. Quantify the impact of transmission type on fuel economy
+#
+# The input for this document is the mtcars data set which comes as part of the
+# datasets package in base R. There is no specific output for this script, the
+# script will just be used as a starting point for creating a markdown file
+# which will later be used to create a final report.
+
+
+#library(dplyr)
+
+
+## Part 0) Loading and preprocessing the data
+
