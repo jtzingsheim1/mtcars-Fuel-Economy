dc6394b33026875a73220c6f4c44161ca6340bfb
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-04-08 22:18:45 -0500

    Completed some initial processing and EDA

--- C7_Project1.R
+++ C7_Project1.R
@@ -17,8 +17,57 @@
 # which will later be used to create a final report.
 
 
-#library(dplyr)
+library(datasets)
+library(dplyr)
 
 
-## Part 0) Loading and preprocessing the data
+## Part 1) Loading, exploring, and preprocessing the data
+
+data("mtcars")  # Load the mt cars data set into workspace as an object mtcars
+mtcars <- as_tibble(mtcars)  # Convert from data frame to a tbl_df object
+#print(str(mtcars))  # Check out the variables and preview some values
+# The variables are mostly self-explanatory, but checking the helpfile is useful
+# mpg	= Miles/(US) gallon
+# cyl	= Number of cylinders
+# disp = Displacement (cu.in.)
+# hp = Gross horsepower
+# drat = Rear axle ratio
+# wt = Weight (1000 lbs)
+# qsec = 1/4 mile time
+# vs = Engine (0 = V-shaped, 1 = straight)
+# am = Transmission (0 = automatic, 1 = manual)
+# gear = Number of forward gears
+# carb = Number of carburetors
+
+# All of the variables begin as the type numeric, but many are not continuous
+# and for the purpose of this analysis it will be more useful to think of them
+# as factors instead. These will be converted to factors below.
+mtcars <- mtcars %>%
+  mutate(cyl = as.factor(cyl)) %>%
+  mutate(vs = as.factor(vs)) %>%
+  mutate(am = as.factor(am)) %>%
+  mutate(gear = as.factor(gear)) %>%
+  mutate(carb = as.factor(carb))
+
+# The vs and am variables are coded with ones and zeros, but it will be easier
+# to work with them by the names those values represent:
+levels(mtcars$vs) <- c("V-shaped", "Straight")
+levels(mtcars$am) <- c("Automatic", "Manual")
+    
+# Make some plots to check out the data
+par(mfrow = c(1, 2))  # Setup plot space
+boxplot(mtcars$mpg ~ mtcars$am, ylab = "mpg [mpg]")  # mpg vs. transmission type
+boxplot(mtcars$wt ~ mtcars$am, ylab = "weight [klbs]")  # wt vs. trans type
+# In the first plot above one could jump to the conclusion that mpg is clearly
+# higher with manual transmission. However in the second plot one can see that
+# manual transmission vehicles tend to be lighter in this dataset, and common
+# sense suggests that mpg should be higher for lower weight vehicles. Many of
+# these variables can be expected to correlate with mpg and/or with each other.
+# The regression model must correct for these confounding effects and correctly
+# isolate the effect of transmission on fuel economy.
+
+
+
+
+
 
