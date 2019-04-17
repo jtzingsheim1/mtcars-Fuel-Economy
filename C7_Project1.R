c96e2d09d135fbb728b0fe656eccbbb095bfdbb2
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-04-17 00:36:57 -0500

    Cleaned up, made and tested initial forward selection functions

--- C7_Project1.R
+++ C7_Project1.R
@@ -24,7 +24,25 @@ library(car)
 
 # Part 0) Function definitions
 
-# Create a function to help with backward elimination modeling method
+# Create a function to help get p-values from models
+SubsetModelSumm <- function(model) {
+  # Subsets a model summary down to its variables and p-values
+  #
+  # Args:
+  #   model: an object containing results returned by a model function (e.g. lm)
+  #
+  # Returns:
+  #   A tibble object of the variables and p-values from summary(lm)
+  model.cf <- summary(model)$coefficients  # Get coefficients section
+  cf.names <- attr(model.cf, "dimnames")[[1]]  # Get variable names
+  model.cf <- as_tibble(model.cf)  # Convert to tibble object
+  model.summ <- model.cf %>%
+    mutate(Variable = cf.names) %>%  # Add column for variable names
+    rename(pvalue = "Pr(>|t|)") %>%
+    select(Variable, pvalue)  # Subset columns
+  return(model.summ)
+}
+
 GetMaxPVariable <- function(model) {
   # Retrieves the variable with the largest p-value from a model
   #
@@ -33,15 +51,31 @@ GetMaxPVariable <- function(model) {
   #
   # Returns:
   #   The largest p-value from a model along with its variable name
-  model.cf <- summary(model)$coefficients  # Get coefficients section
-  cf.names <- attr(model.cf, "dimnames")[[1]]  # Get variable names
-  model.cf <- as_tibble(model.cf)  # Convert to tibble object
-  max.p.var <- model.cf %>%
-    mutate(Variable = cf.names) %>%  # Add column for variable names
-    rename(pvalue = "Pr(>|t|)") %>%
-    select(Variable, pvalue) %>%  # Subset columns
+  model.summ <- SubsetModelSumm(model)
+  max.p.var <- model.summ %>%
     filter(pvalue == max(pvalue))  # Subset rows
   return(as.data.frame(max.p.var))
+}
+
+# Create a function to help with forward selection modeling method
+ShowPValues <- function(model.list) {
+  # Show all variables and p-values from a list of models
+  #
+  # Args:
+  #   model_list: an list containing model objects (e.g. output from lm)
+  #
+  # Returns:
+  #   The variable names and p-values from all the models
+  vars.table <- as_tibble()  # Need to size this or accomplish without loop
+  for (i in seq_along(model.list)) {
+    model.i <- model.list[[i]]  # Get a model from the list
+    model.summ <- SubsetModelSumm(model.i)  # Subset down to vars and pvals
+    model.summ <- model.summ %>%
+      mutate(Model.Num = i) %>%  # Add col to ID the model
+      select(Model.Num, Variable, pvalue)  # Reorder
+    vars.table <- rbind(vars.table, model.summ)  # Trash
+  }
+  return(arrange(vars.table, pvalue))
 }
 
 
@@ -65,13 +99,27 @@ mtcars <- as_tibble(mtcars)  # Convert from data frame to a tbl_df object
 
 # All of the variables are originally loaded as numeric, but some are
 # categorical and others are discrete. The categorical variables should be
-# converted to 
+# converted to factors, and the discrete variables should be assessed.
+#par(mfrow = c(3, 2))  # Setup plot space
+#boxplot(mtcars$mpg ~ mtcars$cyl)
+#plot(mtcars$mpg ~ mtcars$cyl)  # Only three levels, but seems linear
+#boxplot(mtcars$mpg ~ mtcars$gear)
+#plot(mtcars$mpg ~ mtcars$gear)  # Only three levels, and doesn't seem linear
+#boxplot(mtcars$mpg ~ mtcars$carb)
+#plot(mtcars$mpg ~ mtcars$carb)  # Six levels, and somewhat linear, check models
+#test.1 <- lm(mpg ~ carb, data = mtcars)  # Fit a model on carb as numeric
+#test.2 <- lm(mpg ~ as.factor(carb), data = mtcars)  # Fit model on carb as cat.
+#print(summary(test.1))  # R2 = 0.2803 and p-value = 0.0011
+#print(summary(test.2))  # R2 = 0.3377 and p-value = 0.0065
+#rm(test.1, test.2)
+# Based on the checks above, cyl and carb will be treated as numeric and gear
+# will be treated as a factor
+
+# Convert data types
 mtcars <- mtcars %>%
-  #mutate(cyl = as.factor(cyl)) %>%
   mutate(vs = as.factor(vs)) %>%
-  mutate(am = as.factor(am)) #%>%
-  #mutate(gear = as.factor(gear)) %>%
-  #mutate(carb = as.factor(carb))
+  mutate(am = as.factor(am)) %>%
+  mutate(gear = as.factor(gear))
 
 # The vs and am variables are coded with ones and zeros, but it will be easier
 # to work with them by the names those values represent:
@@ -80,40 +128,23 @@ levels(mtcars$am) <- c("Automatic", "Manual")
     
 # Make some plots to check out the data
 par(mfrow = c(1, 2))  # Setup plot space
-#boxplot(mpg ~ am, data = mtcars, ylab = "mpg [mpg]")  # mpg vs transmission type
-#boxplot(wt ~ am, data = mtcars, ylab = "weight [klbs]")  # wt vs. trans type
+#boxplot(mtcars$mpg ~ mtcars$am, ylab = "mpg [mpg]")  # mpg vs trans type
+#boxplot(mtcars$wt ~ mtcars$am, ylab = "weight [klbs]")  # wt vs. trans type
 # In the first plot above one could jump to the conclusion that mpg is clearly
-# larger with manual transmission. However in the second plot one can see that
+# higher with manual transmission. However in the second plot one can see that
 # manual transmission vehicles tend to be lighter in this dataset, and common
 # sense suggests that mpg should be higher for lower weight vehicles. Many of
 # these variables can be expected to correlate with mpg and/or with each other.
 # The regression model must correct for these confounding effects and correctly
-# isolate the effect of transmission on fuel economy.
+# isolate the effect of the transmission type on fuel economy.
 
-# Exploratory plots
-boxplot(mtcars$mpg ~ mtcars$cyl)
-plot(mtcars$mpg ~ mtcars$cyl)
+# Exploratory plots of mpg
 #plot(mtcars$mpg ~ mtcars$disp)
 #plot(mtcars$mpg ~ mtcars$hp)
 #plot(mtcars$mpg ~ mtcars$drat)
 #plot(mtcars$mpg ~ mtcars$wt)
 #plot(mtcars$mpg ~ mtcars$qsec)
-#boxplot(mtcars$mpg ~ mtcars$vs)
-boxplot(mtcars$mpg ~ mtcars$gear)
-plot(mtcars$mpg ~ mtcars$gear)
-boxplot(mtcars$mpg ~ mtcars$carb)
-plot(mtcars$mpg ~ mtcars$carb)
-test1 <- lm(mpg ~ carb, data = mtcars)
-test2 <- lm(mpg ~ as.factor(carb), data = mtcars)
-
-
-#plot(mtcars$hp ~ mtcars$disp)
-#plot(hp ~ wt, data = mtcars)
-#plot(hp ~ disp, data = mtcars)
-#boxplot(mtcars$disp ~ mtcars$am, ylab = "Displacement [in^3]") 
-#boxplot(mtcars$hp ~ mtcars$am, ylab = "Gross Horsepower [hp]")
-#boxplot(mtcars$drat ~ mtcars$am, ylab = "Rear Axle Ratio") 
-#boxplot(mtcars$qsec ~ mtcars$am, ylab = "Quarter Mile Time [s]")
+#boxplot(mtcars$mpg ~ mtcars$vs, ylab = "mpg [mpg]")
 
 # It is useful to pause here and think of the ways in which the variables might
 # relate to each other and fuel economy:
@@ -133,80 +164,97 @@ test2 <- lm(mpg ~ as.factor(carb), data = mtcars)
 # https://shop.advanceautoparts.com/r/advice/car-technology/get-to-know-gear-ratios-and-how-they-affect-acceleration-and-mileage
 # - Weight can be expected to correlate with fuel economy
 # - Quarter mile time is a result of many features of the car similar to mpg,
-# while it can be expected to correlate with mpg, it may not be adding new
-# information to the model.
-# - Engine shape may have a small impact on fuel economy
-# - Number of gears and carburators may correlate with fuel economy
+# while it can be expected to correlate with mpg, it is likely to be very
+# correlated with all the other variables.
+# - Engine shape, number of gears, and number of carburators may all have small
+# correlations with fuel economy
 
-# Check VIFs
-fit_all <- lm(mpg ~ ., data = mtcars)
+# Exploratory plots of interactions
+#boxplot(mtcars$disp ~ mtcars$am, ylab = "Displacement [in^3]") 
+#plot(mtcars$hp ~ mtcars$disp)
+#plot(mtcars$hp ~ mtcars$wt)
+#boxplot(mtcars$hp ~ mtcars$am, ylab = "Gross Horsepower [hp]")
+#boxplot(mtcars$drat ~ mtcars$am, ylab = "Rear Axle Ratio") 
+#boxplot(mtcars$qsec ~ mtcars$am, ylab = "Quarter Mile Time [s]")
 
-# Build a model using backward elimination method
-#print(GetMaxPVariable(fit_all))  # cyl has the highest p-value, eliminate it
-#fit_b1 <- lm(mpg ~ disp + hp + drat + wt + qsec + vs + am + gear + carb,
-#             data = mtcars)
-#print(GetMaxPVariable(fit_b1))  # carb has the highest p-value, eliminate it
-#fit_b2 <- lm(mpg ~ disp + hp + drat + wt + qsec + vs + am + gear, data = mtcars)
-#print(GetMaxPVariable(fit_b2))  # gear has the highest p-value, eliminate it
-#fit_b3 <- lm(mpg ~ disp + hp + drat + wt + qsec + vs + am, data = mtcars)
-#print(GetMaxPVariable(fit_b3))  # vs has the highest p-value, eliminate it
-#fit_b4 <- lm(mpg ~ disp + hp + drat + wt + qsec + am, data = mtcars)
-#print(GetMaxPVariable(fit_b4))  # drat has the highest p-value, eliminate it
-#fit_b5 <- lm(mpg ~ disp + hp + wt + qsec + am, data = mtcars)
-#print(GetMaxPVariable(fit_b5))  # disp has the highest p-value, eliminate it
-#fit_b6 <- lm(mpg ~ hp + wt + qsec + am, data = mtcars)
-#print(GetMaxPVariable(fit_b6))  # hp has the highest p-value, eliminate it
-#fit_b7 <- lm(mpg ~ wt + qsec + am, data = mtcars)
-#print(GetMaxPVariable(fit_b7))  # Intercept has the highest p-value remaining
-
-# Try the exercise again without including qsec
-#fit_b8 <- lm(mpg ~ cyl + disp + hp + drat + wt + vs + am + gear + carb,
-#             data = mtcars)
-#print(GetMaxPVariable(fit_b8))  # carb has the highest p-value, eliminate it
-#fit_b9 <- lm(mpg ~ cyl + disp + hp + drat + wt + vs + am + gear, data = mtcars)
-#print(GetMaxPVariable(fit_b9))  # gear has the highest p-value, eliminate it
-#fit_b10 <- lm(mpg ~ cyl + disp + hp + drat + wt + vs + am, data = mtcars)
-#print(GetMaxPVariable(fit_b10))  # cyl has the highest p-value, eliminate it
-#fit_b11 <- lm(mpg ~ disp + hp + drat + wt + vs + am, data = mtcars)
-#print(GetMaxPVariable(fit_b11))  # drat has the highest p-value, eliminate it
-#fit_b12 <- lm(mpg ~ disp + hp + wt + vs + am, data = mtcars)
-#print(GetMaxPVariable(fit_b12))  # disp has the highest p-value, eliminate it
-#fit_b13 <- lm(mpg ~ hp + wt + vs + am, data = mtcars)
-#print(GetMaxPVariable(fit_b13))  # vs has the highest p-value, eliminate it
-#fit_b14 <- lm(mpg ~ hp + wt + am, data = mtcars)
-#print(GetMaxPVariable(fit_b14))  # am has the highest p-value remaining
-
-# Try a more intelligent starting model
-#fit_b15 <- lm(mpg ~ disp + disp * hp + hp + hp * wt + drat + wt + vs + am + gear + carb,
+# Build a model using the backward elimination method
+#fit.all <- lm(mpg ~ ., data = mtcars)  # Fit a model of all variables, no inter.
+#fit.all <- lm(mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am + gear + carb,
 #              data = mtcars)
-#print(GetMaxPVariable(fit_b15))  # vs has the highest p-value, eliminate it
-#fit_b16 <- lm(mpg ~ disp + disp * hp + hp + hp * wt + drat + wt + am + gear + carb,
+#print(GetMaxPVariable(fit.all))  # cyl
+#fit.b0 <- lm(mpg ~ disp + hp + drat + wt + qsec + vs + am + gear + carb,
 #              data = mtcars)
-#print(GetMaxPVariable(fit_b16))  # Eliminate disp and the disp * hp int. factor
-#fit_b17 <- lm(mpg ~ hp + hp * wt + drat + wt + am + gear + carb, data = mtcars)
-#print(GetMaxPVariable(fit_b17))  # carb has the highest p-value, eliminate it
-#fit_b18 <- lm(mpg ~ hp + hp * wt + drat + wt + am + gear, data = mtcars)
-#print(GetMaxPVariable(fit_b18))  # am has the highest p-value, eliminate it
-#fit_b19 <- lm(mpg ~ hp + hp * wt + drat + wt + gear, data = mtcars)
-#print(GetMaxPVariable(fit_b19))  # drat has the highest p-value, eliminate it
-#fit_b20 <- lm(mpg ~ hp + hp * wt + wt + gear, data = mtcars)
-#print(GetMaxPVariable(fit_b20))  # gear has the highest p-value, eliminate it
-#fit_b21 <- lm(mpg ~ hp + hp * wt + wt, data = mtcars)
-#print(summary(fit_b21))  # All variables have significant p-values
+#print(GetMaxPVariable(fit.b0))  # carb
+#fit.b1 <- lm(mpg ~ disp + hp + drat + wt + qsec + vs + am + gear, data = mtcars)
+#print(GetMaxPVariable(fit.b1))  # gear
+#fit.b2 <- lm(mpg ~ disp + hp + drat + wt + qsec + vs + am, data = mtcars)
+#print(GetMaxPVariable(fit.b2))  # vs
+#fit.b3 <- lm(mpg ~ disp + hp + drat + wt + qsec + am, data = mtcars)
+#print(GetMaxPVariable(fit.b3))  # drat
+#fit.b4 <- lm(mpg ~ disp + hp + wt + qsec + am, data = mtcars)
+#print(GetMaxPVariable(fit.b4))  # disp
+#fit.b5 <- lm(mpg ~ hp + wt + qsec + am, data = mtcars)
+#print(GetMaxPVariable(fit.b5))  # hp
+#fit.b6 <- lm(mpg ~ wt + qsec + am, data = mtcars)
+#print(GetMaxPVariable(fit.b6))  # Intercept
+#fit.b7 <- lm(mpg ~ wt + qsec + am - 1, data = mtcars)
+#print(GetMaxPVariable(fit.b7))  # am
+#fit.b8 <- lm(mpg ~ wt + qsec - 1, data = mtcars)
+#print(GetMaxPVariable(fit.b8))  # wt, but its significant
+#print(summary(fit.b8))
+#anova.b0 <- anova(fit.b8, fit.b7, fit.b6)  # Suggests to keep am and intercept
+#anova.b1 <- anova(fit.b6, fit.b5, fit.b4, fit.b3, fit.b2, fit.b1, fit.b0,
+#                  fit.all)  # Do not keep anything beyond the intercept
+# Based on the sequence above model fit.b6 would be the best to use
 
-# Check if adding any one variable can achieve a significant p-value
+# Original backward method on fit.all:
+# cyl, carb, gear, vs, drat, disp, hp, Intercept
 
-# Try building a model using forward selection without interaction variables
-fit_f1 <- lm(mpg ~ cyl, data = mtcars)
-fit_f2 <- lm(mpg ~ disp, data = mtcars)
-fit_f3 <- lm(mpg ~ hp, data = mtcars)
-fit_f4 <- lm(mpg ~ drat, data = mtcars)
-fit_f5 <- lm(mpg ~ wt, data = mtcars)
-fit_f6 <- lm(mpg ~ qsec, data = mtcars)
-fit_f7 <- lm(mpg ~ vs, data = mtcars)
-fit_f8 <- lm(mpg ~ am, data = mtcars)
-fit_f9 <- lm(mpg ~ gear, data = mtcars)
-fit_f10 <- lm(mpg ~ carb, data = mtcars)
+# Repeated, but starting model excluded qsec:
+#fit.old1 <- lm(mpg ~ cyl + disp + hp + drat + wt + vs + am + gear + carb,
+#              data = mtcars)
+# carb, gear, cyl, drat, disp, vs, am
+
+# Repeated again, but starting model excluded qsec, cyl, and included interact.
+#fit.old2 <- lm(mpg ~ disp + disp * hp + hp + hp * wt + drat + wt + vs + am + gear + carb,
+#              data = mtcars)
+# vs, disp and disp * hp, carb, am, drat, gear
+#fit.old2a <- lm(mpg ~ hp + hp * wt + wt, data = mtcars)
+#print(summary(fit.old2a))  # All variables have significant p-values
+
+# Build a model using forward selection without interaction variables
+fit.f0 <- lm(mpg ~ cyl, data = mtcars)
+fit.f1 <- lm(mpg ~ disp, data = mtcars)
+fit.f2 <- lm(mpg ~ hp, data = mtcars)
+fit.f3 <- lm(mpg ~ drat, data = mtcars)
+fit.f4 <- lm(mpg ~ wt, data = mtcars)
+fit.f5 <- lm(mpg ~ qsec, data = mtcars)
+fit.f6 <- lm(mpg ~ vs, data = mtcars)
+fit.f7 <- lm(mpg ~ am, data = mtcars)
+fit.f8 <- lm(mpg ~ gear, data = mtcars)
+fit.f9 <- lm(mpg ~ carb, data = mtcars)
+models.f0 <- list(fit.f0, fit.f1, fit.f2, fit.f3, fit.f4, fit.f5, fit.f6,
+                  fit.f7, fit.f8, fit.f9)
+#print(ShowPValues(models.f0))  # wt has the lowest p-value
+fit.f10 <- lm(mpg ~ wt + cyl, data = mtcars)
+fit.f11 <- lm(mpg ~ wt + disp, data = mtcars)
+fit.f12 <- lm(mpg ~ wt + hp, data = mtcars)
+fit.f13 <- lm(mpg ~ wt + drat, data = mtcars)
+fit.f14 <- lm(mpg ~ wt + qsec, data = mtcars)
+fit.f15 <- lm(mpg ~ wt + vs, data = mtcars)
+fit.f16 <- lm(mpg ~ wt + am, data = mtcars)
+fit.f17 <- lm(mpg ~ wt + gear, data = mtcars)
+fit.f18 <- lm(mpg ~ wt + carb, data = mtcars)
+models.f1 <- list(fit.f10, fit.f11, fit.f12, fit.f13, fit.f14, fit.f15, fit.f16,
+                  fit.f17, fit.f18)
+print(ShowPValues(models.f1), n = 20)  # cyl has the lowest p-value
+
+
+
+
+
+
+
 
 
 
